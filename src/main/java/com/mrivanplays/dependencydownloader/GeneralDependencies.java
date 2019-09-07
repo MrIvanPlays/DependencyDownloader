1fdec2002177fc4e055841861cc017c3ec3427b0
MrIvanPlays <pekov.ivan@abv.bg>
2019-09-07 18:01:23 +0300

    Add more general dependencies, bump version

--- src/main/java/com/mrivanplays/dependencydownloader/GeneralDependencies.java
+++ src/main/java/com/mrivanplays/dependencydownloader/GeneralDependencies.java
@@ -22,23 +22,51 @@ package com.mrivanplays.dependencydownloader;
  * the main jar
  */
 public enum GeneralDependencies {
+    // database drivers
     MONGO_JAVA_DRIVER("org.mongodb", "mongo-java-driver", "3.9.1"),
     MONGO_MORPHIA("org.mongodb", "morphia", "1.3.2"),
     MARIADB_JAVA_CLIENT("org.mariadb.jdbc", "mariadb-java-client", "2.4.3"),
-    JEDIS("redis.clients", "jedis", "3.1.0");
+    JEDIS("redis.clients", "jedis", "3.1.0"),
+    // discord bot stuff
+    JDA("net.dv8tion", "jda", "4.0.0_46", "http://jcenter.bintray.com/"),
+    JAVACORD("org.javacord", "javacord", "3.0.4"),
+    DISCORDJ4("com.discord4j", "discord4j-core", "3.0.8"), // discord4j really sucks. better using jda or javacord
+    // google stuff
+    GUICE("com.google.inject", "guice", "4.0.0"),
+    GUAVA("com.google.guava", "guava", "28.1-jre"), // if you don't want to use the spigot one
+    // asm stuff
+    ASM("org.ow2.asm", "asm", "7.0"),
+    ASM_ANALYSIS("org.ow2.asm", "asm-analysis", "7.0"),
+    ASM_COMMONS("org.ow2.asm", "asm-commons", "7.0"),
+    ASM_TREE("org.ow2.asm", "asm-tree", "7.0"),
+    ASM_UTIL("org.ow2.asm", "asm-util", "7.0"),
+    // specifically minecraft related stuff
+    COMMODORE("me.lucko", "commodore", "1.3"),
+    ADVANCEMENT_CREATOR("com.github.Trigary", "AdvancementCreator", "v2.0", "https://jitpack.io");
 
     private final String groupId;
     private final String artifactId;
     private final String version;
+    private final String repositoryUrl;
 
     GeneralDependencies(
             String groupId,
             String artifactId,
             String version
     ) {
+        this(groupId, artifactId, version, "http://repo1.maven.org/maven2");
+    }
+
+    GeneralDependencies(
+            String groupId,
+            String artifactId,
+            String version,
+            String repositoryUrl
+    ) {
         this.groupId = groupId;
         this.artifactId = artifactId;
         this.version = version;
+        this.repositoryUrl = repositoryUrl;
     }
 
     /**
@@ -47,7 +75,7 @@ public enum GeneralDependencies {
      * @return dependency
      */
     public Dependency asDependency() {
-        return new Dependency(groupId, artifactId, version, "http://repo1.maven.org/maven2");
+        return new Dependency(groupId, artifactId, version, repositoryUrl);
     }
 
     /**
